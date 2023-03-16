0da9405b4f32c8e42f5878289afb3dbb44ffce20
Viet Tran <viettran101294@gmail.com>
2023-03-15 20:31:31 -0400

    first commit

---
+++ src/app/store.ts
@@ -0,0 +1,23 @@
+import { configureStore, ConfigureStoreOptions } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { api } from './services/api';
+
+
+export const createStore = (
+  options?: ConfigureStoreOptions['preloadedState'] | undefined
+) =>
+  configureStore({
+    reducer: {
+      [api.reducerPath]: api.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(api.middleware),
+    ...options,
+  });
+
+export const store = createStore();
+
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
