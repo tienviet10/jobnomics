193f2352c7388e45fef368b6f1b2fff5e9c4fae6
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-03-16 20:13:34 -0400

    Make Job Modal Container Static Component

--- src/components/JobModal/JobModal.tsx
+++ src/components/JobModal/JobModal.tsx
@@ -1,14 +1,52 @@
-import { Typography, Modal, Box } from "@mui/material";
+import { Typography, Modal, IconButton, Card } from "@mui/material";
 
-import React from "react";
+import React, { useState } from "react";
+
+import styles from "./JobModal.module.css";
+import { Close, Delete, Favorite } from "@mui/icons-material";
 
 const JobModal = () => {
-  const open = true;
+  const [open, setOpen] = useState(true);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <Modal open={open}>
-      <Box>
-        <Typography>JobModal</Typography>
-      </Box>
+    <Modal
+      open={open}
+      onClose={handleClose}
+      className={styles.JobModalContainer}
+    >
+      <Card elevation={5} className={styles.JobModal}>
+        <div className={styles.ModalHeader}>
+          <div className={styles.JobHeader}>
+            <Typography variant="h3" className={styles.JobTitle}>
+              Title
+            </Typography>
+            <Typography variant="h5" className={styles.JobCompany}>
+              Company Name | Location
+            </Typography>
+            <Typography variant="subtitle1" className={styles.JobUpdatedDate}>
+              03/16/2023
+            </Typography>
+          </div>
+          <IconButton className={styles.Button}>
+            <Close fontSize="large" />
+          </IconButton>
+        </div>
+        {/* View Changes in here */}
+        <div className={styles.ModalFooter}>
+          <IconButton className={styles.Button}>
+            <Typography className={styles.ButtonText}>Favorite</Typography>
+            <Favorite fontSize="large" />
+          </IconButton>
+          <IconButton className={styles.Button}>
+            <Typography className={styles.ButtonText}>Delete</Typography>
+            <Delete fontSize="large" />
+          </IconButton>
+        </div>
+      </Card>
     </Modal>
   );
 };
--- src/pages/JobPage/JobPage.tsx
+++ src/pages/JobPage/JobPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import JobList from "../../components/JobList";
+import JobModal from "../../components/JobModal";
 import styles from "./JobPage.module.css";
 
 import { Typography } from "@mui/material";
@@ -12,6 +13,7 @@ const JobPage = () => {
         (User's) Job Board
       </Typography>
       <JobList />
+      <JobModal />
     </div>
   );
 };
