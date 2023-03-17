5a4486835d2fca79a1fd1d5a11e2d4cdd0c6624f
Viet Tran <viettran101294@gmail.com>
2023-03-17 12:27:05 -0400

    Complete refactor Modal Wrapper for later use in different views

--- src/components/JobModal/JobModal.tsx
+++ src/components/JobModal/JobModal.tsx
@@ -1,20 +1,9 @@
-import { Typography, Modal, IconButton, Card, dividerClasses } from "@mui/material";
-
-import React, { useEffect } from "react";
-import type { RootState } from "../../app/store";
-
-import { Close, Delete, Favorite, FavoriteBorder } from "@mui/icons-material";
-
-import {
-  toggleFavorite,
-} from "../../features/jobSlice";
-import {
-  useUpdateJobMutation,
-} from "../../app/services/job-api";
-import { useDispatch, useSelector } from "react-redux";
-
+import React from "react";
 import styles from "./JobModal.module.css";
+
 import ModalWrapper from "../Modal";
+import { RootState } from "../../app/store";
+import { useSelector } from "react-redux";
 
 const JobModal = () => {
   const state = useSelector((state: RootState) => state.job);
@@ -22,7 +11,7 @@ const JobModal = () => {
 
   return (
     <ModalWrapper>
-      
+      {/* from category -> display modal views accordingly */}
     </ModalWrapper>
   );
 };
--- src/components/Modal/ModalWrapper.tsx
+++ src/components/Modal/ModalWrapper.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import { Typography, Modal, IconButton, Card } from "@mui/material";
-import type { RootState } from "../../app/store";
-import { Close} from "@mui/icons-material";
 import {
+  toggleFavorite,
   toggleJobModal,
+  setSelectedJob,
 } from "../../features/jobSlice";
+import {
+  useGetJobByIdQuery,
+  useUpdateJobMutation,
+} from "../../app/services/job-api";
 import { useDispatch, useSelector } from "react-redux";
-import styles from "./JobModal.module.css";
+import { Close, Delete, Favorite, FavoriteBorder } from "@mui/icons-material";
 
-const ModalWrapper = ({children}: { children: React.ReactNode }) => {
+import type { RootState } from "../../app/store";
+import styles from "./MainModal.module.css";
+
+
+const ModalWrapper = ({ children }: { children: React.ReactNode; }) => {
   const dispatch = useDispatch();
 
   const state = useSelector((state: RootState) => state.job);
@@ -18,8 +26,41 @@ const ModalWrapper = ({children}: { children: React.ReactNode }) => {
     ? new Date(selectedJob.updatedAt)
     : "";
 
+  const { userId, jobId, categoryId } = modalState.userJobId;
+
+  const { data, error, isLoading } = useGetJobByIdQuery({
+    userId,
+    jobId,
+    categoryId,
+  });
+  const [updateJob, { isLoading: isUpdating }] = useUpdateJobMutation();
+
   const handleClose = () => {
     dispatch(toggleJobModal(false));
+  };
+
+  useEffect(() => {
+    dispatch(setSelectedJob(data));
+  }, [data]);
+
+
+  const handleToggleFavorite = () => {
+    const body = {
+      userId: 1,
+      jobId,
+      categoryId,
+      favorite: !selectedJob.isFavorite,
+      interviewDate: selectedJob.interviewDate,
+      type: "update",
+    };
+    updateJob(body);
+    dispatch(
+      toggleFavorite([
+        selectedJob.category.name,
+        selectedJob.job.id,
+        !selectedJob.isFavorite,
+      ])
+    );
   };
 
   return selectedJob ? (
@@ -46,11 +87,25 @@ const ModalWrapper = ({children}: { children: React.ReactNode }) => {
           </IconButton>
         </div>
         {children}
+        <div className={styles.ModalFooter}>
+          <IconButton className={styles.Button} onClick={handleToggleFavorite}>
+            <Typography className={styles.ButtonText}>Favorite</Typography>
+            {selectedJob?.isFavorite ? (
+              <Favorite fontSize="medium" />
+            ) : (
+              <FavoriteBorder fontSize="medium" />
+            )}
+          </IconButton>
+          <IconButton className={styles.Button}>
+            <Typography className={styles.ButtonText}>Delete</Typography>
+            <Delete fontSize="medium" />
+          </IconButton>
+        </div>
       </Card>
     </Modal>
   ) : (
     <></>
   );
-}
+};
 
-export default ModalWrapper
+export default ModalWrapper;
