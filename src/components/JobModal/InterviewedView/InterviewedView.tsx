7589cd9685c8a010f2a92aa22dac70c9f5ffcb42
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-03-17 22:04:47 -0400

    Add progress bar

--- src/components/JobModal/InterviewedView/InterviewedView.tsx
+++ src/components/JobModal/InterviewedView/InterviewedView.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { useSelector, useDispatch } from "react-redux";
 
@@ -7,11 +7,21 @@ import type { RootState } from "../../../app/store";
 import { toggleCheckbox } from "../../../features/jobSlice";
 
 import styles from "./InterviewedView.module.css";
-import { Checkbox, FormControlLabel, FormGroup } from "@mui/material";
+import {
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  LinearProgress,
+  Tab,
+  Tabs,
+  Typography,
+} from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 const InterviewedView = (): JSX.Element => {
   const dispatch = useDispatch();
   const [isNotepad, setIsNotepad] = useState(false);
+  const [progressMessage, setProgressMessage] = useState("Reminder for you");
 
   const selectedJobState = useSelector(
     (state: RootState) => state.job.selectedJob
@@ -34,6 +44,26 @@ const InterviewedView = (): JSX.Element => {
     (checklist) => checklist.isComplete
   ).length;
 
+  const progress = Math.round(
+    (numberOfCompleted / selectedJobState.checklists.length) * 100
+  );
+
+  useEffect(() => {
+    let message = "";
+    if (progress >= 0 && progress <= 20) {
+      message = "Good start!";
+    } else if (progress > 20 && progress <= 40) {
+      message = "Great job!";
+    } else if (progress > 40 && progress <= 60) {
+      message = "Keep it up!";
+    } else if (progress > 60 && progress <= 80) {
+      message = "You're almost done!";
+    } else if (progress === 100) {
+      message = "Congrats you did it!";
+    }
+    setProgressMessage(message);
+  }, [progress]);
+
   const checklists = selectedJobState.checklists.map((checklist) => {
     return (
       <FormControlLabel
@@ -42,7 +72,7 @@ const InterviewedView = (): JSX.Element => {
           <Checkbox
             id={String(checklist.id)}
             checked={checklist.isComplete}
-            color="success"
+            color={progress === 100 ? "success" : "primary"}
             onChange={handleToggleChecklist}
           />
         }
@@ -51,10 +81,40 @@ const InterviewedView = (): JSX.Element => {
     );
   });
 
+  const handleTabChange = () => {
+    setIsNotepad(true);
+  };
+
   return (
-    <div className={styles.InterviewedContainer}>
-      <FormGroup>{checklists}</FormGroup>
-    </div>
+    <>
+      <Tabs value={isNotepad} onChange={handleTabChange} centered>
+        <Tab label="Checklist" />
+        <Tab label="Note Pad" />
+      </Tabs>
+      <div className={styles.InterviewedContainer}>
+        <Typography
+          variant="h5"
+          color={progress === 100 ? "#1b5e20" : "primary"}
+          className={styles.ProgressMessage}
+        >
+          {progressMessage}
+          {progress === 100 && (
+            <CheckCircleIcon
+              fontSize="large"
+              color="success"
+              sx={{ marginLeft: "10px" }}
+            />
+          )}
+        </Typography>
+        <LinearProgress
+          variant="determinate"
+          value={progress}
+          color={progress === 100 ? "success" : "primary"}
+          className={styles.CheckboxProgressBar}
+        />
+        <FormGroup>{checklists}</FormGroup>
+      </div>
+    </>
   );
 };
 
