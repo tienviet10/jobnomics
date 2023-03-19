61db53865e7ad7a1c6b2e2472dbb20fec0e66365
Viet Tran <viettran101294@gmail.com>
2023-03-19 00:11:45 -0400

    Resolve conflicts when merging

--- src/pages/Search/manage-search-page.ts
+++ src/pages/Search/manage-search-page.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useFilterJobsQuery } from '../../app/services/job-api';
@@ -7,57 +6,15 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../../app/store';
 import { useDispatch } from 'react-redux';
 import { setList, toggleCheck } from '../../features/filterSlice';
-=======
-import React, { useEffect, useState } from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-import { useFilterJobMutation } from "../../app/services/job-api";
-import {
-  CheckBoxEntity,
-  ManageSearchPageType,
-  ResponseData,
-  UpdateFilterType,
-} from "../../types/jobTypes";
-import { useSelector } from "react-redux";
-import { RootState } from "../../app/store";
-import { useDispatch } from "react-redux";
-import {
-  setList,
-  toggleCheck,
-  toggleFirstFetch,
-} from "../../features/filterSlice";
->>>>>>> main
+
 
 export function useManageSearchPage(): ManageSearchPageType {
   const { logout } = useAuth0();
   const dispatch = useDispatch();
-<<<<<<< HEAD
   const filterState = useSelector((state: RootState) => state.filter.mainFilter);
+  // If you want to make multiple GET request with different params, you can useState to get new information and store in the same cache name (like 'aJob')
   const [queryStr, setQueryStr] = useState<any>({ category: ["Bookmarked", "Applied", "Interviewing", "Interviewed", "Job Offer", "Position Filled"], skills: [] });
   const { data, isLoading } = useFilterJobsQuery(queryStr);
-=======
-  const filterState = useSelector(
-    (state: RootState) => state.filter.mainFilter
-  );
-  const [filterJob] = useFilterJobMutation();
-  const [request, setRequest] = useState(false);
-
-  const prefetchData = async () => {
-    const res: ResponseData = await filterJob({
-      category: [
-        "Bookmarked",
-        "Applied",
-        "Interviewing",
-        "Interviewed",
-        "Job Offer",
-        "Position Filled",
-      ],
-      skills: [],
-    });
-    if (res.data) {
-      dispatch(setList(res.data));
-    }
-  };
->>>>>>> main
 
   useEffect(() => {
     if (data) {
@@ -81,37 +38,15 @@ export function useManageSearchPage(): ManageSearchPageType {
   };
 
   const sentFilterRequest = async () => {
-    const newCategory = filterState.category
-      .filter((obj: CheckBoxEntity) => obj.check)
-      .map((obj: CheckBoxEntity) => obj.name);
-    const languagesAndFramework = filterState.languages
-      .concat(filterState.framework)
-      .filter((obj: CheckBoxEntity) => obj.check)
-      .map((obj: CheckBoxEntity) => obj.name);
+    const newCategory = filterState.category.filter((obj: CheckBoxEntity) => obj.check).map((obj: CheckBoxEntity) => obj.name);
+    const languagesAndFramework = filterState.languages.concat(filterState.framework).filter((obj: CheckBoxEntity) => obj.check).map((obj: CheckBoxEntity) => obj.name);
 
-<<<<<<< HEAD
     setQueryStr({ category: newCategory, skills: languagesAndFramework });
-=======
-    const res: ResponseData = await filterJob({
-      category: newCategory,
-      skills: languagesAndFramework,
-    });
-
-    if (res.data) {
-      dispatch(toggleFirstFetch(false));
-      dispatch(setList(res.data));
-    }
->>>>>>> main
   };
 
   return {
     updateCategoryFilter,
     logout,
-<<<<<<< HEAD
     sentFilterRequest
-=======
-    sentFilterRequest,
-    prefetchData,
->>>>>>> main
   };
 }
