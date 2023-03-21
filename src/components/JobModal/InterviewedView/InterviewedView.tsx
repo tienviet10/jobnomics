e6d66349aa30cec61ce856bac34e129c692be45d
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-03-21 13:39:36 -0400

    Make checklist modal responsive

--- src/components/JobModal/InterviewedView/InterviewedView.tsx
+++ src/components/JobModal/InterviewedView/InterviewedView.tsx
@@ -9,6 +9,7 @@ import { setNewNote, toggleCheckbox } from "../../../features/jobSlice";
 
 import styles from "./InterviewedView.module.css";
 import {
+  Box,
   Button,
   Checkbox,
   FormControlLabel,
@@ -34,7 +35,7 @@ const InterviewedView = (): JSX.Element => {
   const [saveNote] = useUpdateNoteMutation();
   const [updateChecklist] = useUpdateChecklistMutation();
 
-  const { selectedJob } = useGetAJob();
+  const { selectedJob, skills } = useGetAJob();
 
   const handleToggleChecklist = (event: { target: { id: string } }) => {
     const checkboxId = Number(event.target.id);
@@ -61,7 +62,7 @@ const InterviewedView = (): JSX.Element => {
     const numberOfCompleted = selectedJob.checklists.filter(
       (checklist: Checklist) => checklist.isComplete
     ).length;
-  
+
     const progress = Math.round(
       (numberOfCompleted / selectedJob.checklists?.length) * 100
     );
@@ -80,10 +81,10 @@ const InterviewedView = (): JSX.Element => {
     }
     setProgress(progress);
     setProgressMessage(message);
-  }
+  };
 
   useEffect(() => {
-    progressBar()
+    progressBar();
   }, [selectedJob.checklists]);
 
   const checklists = selectedJob.checklists.map((checklist: Checklist) => {
@@ -98,13 +99,17 @@ const InterviewedView = (): JSX.Element => {
             onChange={handleToggleChecklist}
           />
         }
-        label={checklist.description}
+        label={
+          <Typography sx={{ fontSize: { xs: "13px", sm: "18px" } }}>
+            {checklist.description}
+          </Typography>
+        }
       />
     );
   });
 
   const handleTabChange = () => {
-    setIsNotepad(!isNotepad)
+    setIsNotepad(!isNotepad);
   };
 
   const handleNoteChange = (event: {
@@ -122,7 +127,7 @@ const InterviewedView = (): JSX.Element => {
   };
 
   return (
-    <div className={styles.InterviewedContainer}>
+    <Box className={styles.InterviewedContainer}>
       <Tabs
         value={isNotepad ? 1 : 0}
         onChange={handleTabChange}
@@ -132,56 +137,88 @@ const InterviewedView = (): JSX.Element => {
         <Tab label="Checklist" />
         <Tab label="Note Pad" />
       </Tabs>
-      <section className={styles.InterviewedMain}>
-        {!isNotepad ? (
-          <>
-            <Typography
-              variant="h5"
-              color={progress === 100 ? "#1b5e20" : "primary"}
-              className={styles.ProgressMessage}
-            >
-              {progressMessage}
-              {progress === 100 && (
-                <CheckCircle
-                  fontSize="large"
-                  color="success"
-                  sx={{ marginLeft: "10px" }}
-                />
-              )}
-            </Typography>
-            <LinearProgress
-              variant="determinate"
-              value={progress}
-              color={progress === 100 ? "success" : "primary"}
-              className={styles.CheckboxProgressBar}
-            />
-            <FormGroup>{checklists}</FormGroup>
-          </>
-        ) : (
-          <>
-            <Typography variant="h5">
-              Keep track of your interview experince.
-            </Typography>
-            <Typography variant="body2" gutterBottom>
-              What questions did you get? How did you respond? What did you
-              learn? How do you feel about this company? Did you receive any
-              feedback? What can you do better next time?
-            </Typography>
-            <TextField
-              className={styles.NoteTextField}
-              sx={{ "& fieldset": { outline: "none" } }}
-              rows={10}
-              placeholder="Write your notes here..."
-              multiline
-              fullWidth
-              value={selectedJob.note}
-              onChange={handleNoteChange}
-            ></TextField>
-            <Button onClick={handleSaveNote}>Save</Button>
-          </>
-        )}
-      </section>
-    </div>
+      {!isNotepad ? (
+        <Box
+          className={styles.ChecklistMain}
+          sx={{
+            height: { xs: "40vh", sm: "40vh" },
+            width: "100%",
+            px: { xs: "0", sm: "40px", md: "50px" },
+          }}
+        >
+          <Typography
+            variant="h5"
+            color={progress === 100 ? "#1b5e20" : "primary"}
+            className={styles.ProgressMessage}
+            sx={{ fontSize: { xs: "20px", sm: "24px" }, mt: 2 }}
+          >
+            {progressMessage}
+            {progress === 100 && (
+              <CheckCircle
+                fontSize="large"
+                color="success"
+                sx={{
+                  marginLeft: "10px",
+                  position: "absolute",
+                  bottom: 0,
+                  left: "98%",
+                }}
+              />
+            )}
+          </Typography>
+          <LinearProgress
+            variant="determinate"
+            value={progress}
+            color={progress === 100 ? "success" : "primary"}
+            className={styles.CheckboxProgressBar}
+            sx={{
+              p: { xs: "5px", sm: "8px" },
+              width: { xs: "100%", sm: "80%" },
+              my: { xs: 3 },
+            }}
+          />
+          <FormGroup className={styles.Checklist}>{checklists}</FormGroup>
+        </Box>
+      ) : (
+        <Box
+          className={styles.InterviewedMain}
+          sx={{ px: { xs: "15px", sm: "40px", md: "50px" }, py: 2, pb: 0 }}
+        >
+          <Typography variant="h5">
+            Keep track of your interview experince.
+          </Typography>
+          <Typography variant="body2" gutterBottom>
+            What questions did you get? How did you respond? What did you learn?
+            How do you feel about this company? Did you receive any feedback?
+            What can you do better next time?
+          </Typography>
+          <TextField
+            className={styles.NoteTextField}
+            sx={{ "& fieldset": { outline: "none" } }}
+            rows={10}
+            placeholder="Write your notes here..."
+            multiline
+            fullWidth
+            value={selectedJob.note}
+            onChange={handleNoteChange}
+          ></TextField>
+          <Button onClick={handleSaveNote}>Save</Button>
+        </Box>
+      )}
+
+      {selectedJob?.job?.skills && (
+        <Typography
+          variant="subtitle1"
+          className={styles.Skills}
+          sx={{
+            fontSize: { xs: "13px", md: "16px" },
+            lineHeight: { xs: "16px", md: "20px" },
+          }}
+        >
+          <strong>Required Skills:</strong> {skills}
+        </Typography>
+      )}
+    </Box>
   );
 };
 
