c8c925a1df868341407f144cace0d90149c70c1d
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-03-23 14:42:06 -0400

    Fix inactiveModal style and jobpage calendar button

--- src/pages/JobPage/JobPage.tsx
+++ src/pages/JobPage/JobPage.tsx
@@ -11,6 +11,7 @@ import JobModal from "../../components/JobModal";
 import InterviewDateModal from "../../components/InterviewDateModal";
 import InactiveJobsModal from "../../components/InactiveJobsModal.tsx";
 import CalendarModal from "../../components/Calendar";
+import { CalendarMonth } from "@mui/icons-material";
 
 const JobPage = () => {
   const { user } = useAuth0();
@@ -26,24 +27,32 @@ const JobPage = () => {
 
   return (
     <div className={styles.JobPage}>
-      <Typography variant="h5" className={styles.JobPageTitle}>
-        <span>
-          {user?.name ||
-            user?.given_name ||
-            user?.family_name ||
-            user?.nickname}
-          's{" "}
-        </span>
-        <span>Job Board</span>
-        <Button onClick={()=> setCalendarOpen(true)}>Calendar</Button>
-      </Typography>
+      <div className={styles.JobPageHeader}>
+        <Typography variant="h5" className={styles.JobPageTitle}>
+          <span>
+            {user?.name ||
+              user?.given_name ||
+              user?.family_name ||
+              user?.nickname}
+            's{" "}
+          </span>
+          <span>Job Board</span>
+        </Typography>
+        <Button
+          onClick={() => setCalendarOpen(true)}
+          sx={{ display: "flex", flexDirection: "column" }}
+        >
+          <CalendarMonth fontSize="large" sx={{ ml: 0.3 }} />
+          <Typography variant="caption">Calendar</Typography>
+        </Button>
+      </div>
       <JobList />
       <InterviewDateModal />
       <InactiveJobsModal
         open={openInactiveModal}
         setOpen={setOpenInactiveModal}
       />
-      <CalendarModal open={calendarOpen} setOpen={setCalendarOpen}/>
+      <CalendarModal open={calendarOpen} setOpen={setCalendarOpen} />
       <JobModal />
     </div>
   );
