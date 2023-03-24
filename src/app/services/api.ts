aa2a8b9693fd1a3839d556353063618e43f0f300
Viet Tran <viettran101294@gmail.com>
2023-03-24 16:08:09 -0400

    Add user information to the database

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -32,7 +32,7 @@ export const api = createApi({
    * Tag types must be defined in the original API definition
    * for any tags that would be provided by injected endpoints
    */
-  tagTypes: ['Time', 'Posts', 'Counter'],
+  // tagTypes: ['Time', 'Posts', 'Counter'],
   /**
    * This api has endpoints injected in adjacent files,
    * which is why no endpoints are shown below.
