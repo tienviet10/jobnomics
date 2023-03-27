e8b29ec0228a46ab2f4e7e60119b5b1b936d229c
Viet Tran <viettran101294@gmail.com>
2023-03-26 21:28:11 -0400

    Add scope and audience to auth0

--- src/components/auth/Auth0ProviderWithNavigate.tsx
+++ src/components/auth/Auth0ProviderWithNavigate.tsx
@@ -28,7 +28,7 @@ const Auth0ProviderWithNavigate = ({ children }: { children: React.ReactNode }) 
       }}
       onRedirectCallback={onRedirectCallback}
       useRefreshTokens={true}
-      // useRefreshTokensFallback={true}
+      useRefreshTokensFallback={true}
     >
       {children}
     </Auth0Provider>
